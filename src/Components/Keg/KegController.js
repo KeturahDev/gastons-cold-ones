b96655bc2c1b3a9f3ba2de711f86f7757d5cfd4e
Keturah Howard <keturah.dev@gmail.com>
2020-04-24 10:14:12 -0700

    add conditional rendering for keg comoponents within keg control

--- src/Components/Keg/KegController.js
+++ src/Components/Keg/KegController.js
@@ -7,18 +7,43 @@ class KegController extends React.Component{
   constructor(props){
     super(props)
     this.state={
+      formVisible: false,
+      selectedKeg: null,
+      // kegList: 
+    }
+  }
+
+
+  setVisiblitiy = () => {
+    if(this.state.selectedKeg !== null) {
+      return{
+        component: <KegDetails/>,
+        buttonText: "Back to Kegs"
+      }
+    } else if (this.state.formVisible){
+      return{
+        component: <KegForm/>,
+        buttonText: "Back to Kegs"
+      };
+    } else {
+      return{
+        component: <KegList/>,
+        buttonText: "Add new Keg"
+      }
     }
   }
 
   render(){
     //conditional for rendering KegList, KegDetails, KegForm
-
+    const currentlyVisible = this.setVisiblitiy()
     return(
       <React.Fragment>
-        <div>
-          <KegList />
+        <div style={{border: "2px solid gray", padding: "2%"}}>
+          {/* <KegList />
           <KegDetails />
-          <KegForm />
+          <KegForm /> */}
+          {currentlyVisible.component}
+          <button>{currentlyVisible.buttonText}</button>
         </div>
       </React.Fragment>
     );
