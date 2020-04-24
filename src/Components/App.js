2f305b640404b2e01f04c18029d2e1eb896932d4
Keturah Howard <keturah.dev@gmail.com>
2020-04-24 08:55:47 -0700

    rearange boilerplate file layout

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">
+        <header className="App-header">
+        </header>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
