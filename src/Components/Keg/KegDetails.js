996adf80249f4862050be4a4cfed91f62c514219
Keturah Howard <keturah.dev@gmail.com>
2020-04-24 13:25:00 -0700

    display keg information details page

--- src/Components/Keg/KegDetails.js
+++ src/Components/Keg/KegDetails.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function KegDetails(){
+function KegDetails(props){
+  const { keg } = props
   return(
     <React.Fragment>
       <div style={{border: "2px solid purple"}}>
-        Detail of Selected Keg
+        <h2>{keg.name}</h2>
+        <p>{keg.description}</p>
+        <p>{keg.band}</p>
+        <p>{keg.price}</p>
+        <p>{keg.alcoholContent}</p>
+        <p>{keg.pints}</p>
       </div>
     </React.Fragment>
   );
 }
 
-
+KegDetails.propTypes = {
+  keg: PropTypes.object
+}
 export default KegDetails;
