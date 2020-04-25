f0679db2749b32bf6bbb30739a64f8dda459814a
Keturah Howard <keturah.dev@gmail.com>
2020-04-24 20:31:06 -0700

    Updates

---
+++ precache-manifest.0493e027c7f82a66a94bd8ad957909de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5ee5c9c22a34ffa2f28beb840e548a",
+    "url": "/gastons-cold-ones/index.html"
+  },
+  {
+    "revision": "9ab503ac5628bb9e9a79",
+    "url": "/gastons-cold-ones/static/css/main.0311d9fe.chunk.css"
+  },
+  {
+    "revision": "9385e59a2ed00ecffc34",
+    "url": "/gastons-cold-ones/static/js/2.87cec374.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gastons-cold-ones/static/js/2.87cec374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab503ac5628bb9e9a79",
+    "url": "/gastons-cold-ones/static/js/main.e27d281f.chunk.js"
+  },
+  {
+    "revision": "a982610387a12635b140",
+    "url": "/gastons-cold-ones/static/js/runtime-main.3b6cc763.js"
+  }
+]);
